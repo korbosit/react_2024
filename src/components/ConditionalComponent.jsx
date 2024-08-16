ae3ac188da0565a707f6552ab354a06aabebb5cd
Vlaimir Korba <krissky1986@gmail.com>
2024-08-16 10:24:09 +0300

    #18 - Using Ternary Operators In React

--- src/components/ConditionalComponent.jsx
+++ src/components/ConditionalComponent.jsx
@@ -2,14 +2,7 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function ConditionalComponent() {
-    let messageOne;
-    let messageOTwo;
-    let message;
     const display = false;
-    if (display) {
-        message = <h1>This is message 1</h1>;
-    } else {
-        message = <h1>This is message 2</h1>;
-    }
-    return message;
+
+    return display ? <Welcome /> : <Code />;
 }
